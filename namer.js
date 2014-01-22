71aa5ddb90c21f4f15e92086fa41451fb892e7e5
grrrwaaa <grrrwaaa@gmail.com>
2014-01-23 05:12:58 +0900

    layout fix

--- namer.js
+++ namer.js
@@ -5,55 +5,62 @@ var consonants = [
 ];
 
 var vowels = [
-	"a", "e", "i", "o", "u", "ae", "ai", "ao", "au", "ay", "ea", "ee", "ei", "eu", "ie", "io", "iu", "oa", "oi", "oo", "ou", "ya", "ye", "yo", "yu", 
-	"ar", "er", "ir", "or", "ur", "aer", "air", "ear", "eur", "ier", "ior", "iur", "oar", "our"
+	"a", "e", "i", "o", "u", 
+	"ae", "ai", "ao", "au", "ay", "ea", "ee", "ei", "eu", "ie", "io", "iu", "oa", "oi", "oo", "ou", 
+	"ya", "ye", "yo", "yu", 
+	"ar", "er", "ir", "or", "ur", 
+	//"aer", "air", "ear", "eur", "ier", "ior", "iur", "oar", "our"
 ];
 
 var startings = [
-	"a", "aa", "acti", "ae", "aer", "ai", "ana", "ao", "ar", "au", 
+	"a", "aa", "acti", "ae", "aer", "ai", "ana", "ano", "anti", "ao", "ar", "au", 
 	"ba", "bo", "boo", "by", 
-	"cen", "cho", "chro", "cla", "co", "coa", "coi", 
-	"de", "digi", "dri", 
-	"e", "ea", "ear", "ei", "enve", "eu", 
-	"fa", "fee", "fi", "fla", "fri", 
+	"cen", "cho", "cha", "chro", "cla", "co", "coa", "coi", "cry", 
+	"de", "digi", "dio", "do", "dri", 
+	"e", "ea", "ear", "ei", "ele", "enve", "eu", 
+	"fa", "fee", "fi", "fla", "for", "free", "fri", 
+	"gi", "gra", 
 	"hi", 
-	"i", "in", "ir", 
+	"i", "in", "inter", "ir", 
 	"le", "lo",  
-	"ma", "maxi", "medi", "mega", "mi", "mini", "mo", 
-	"na", "ne", "nega", 
-	"o", "omni", "opto", "or", "osci", "ou",
-	"para", "pea", "pha", "pi", "pli", "pro", "pu",
-	"ra", "re", "redu", 
-	"satu", "see", "sha", "so", "spa", "spe", "sta", "ste", "sy", 
-	"te", "thra", "thri", "tri", "tu", 
+	"ma", "medi", "mega", "mi", "mo", 
+	"na", "ne", "nega", "no", 
+	"o", "omni", "opto", "or", "orbi", "orga", "osci", "ou",
+	"para", "pea", "pha", "phase", "pi", "pli", "pri", "pro", "pu",
+	"ra", "re", "redu", "resi", 
+	"satu", "see", "sha", "shi", "so", "spa", "spe", "sta", "ste", "sy", 
+	"te", "thra", "thri", "thu", "tra", "tri", "tu", 
 	"u", "ur", 
-	"vi", "voi", 
+	"ve", "vi", "voi", 
 	"wa", "we", "wi", 
 ];
 
 var vendings = [
-	"back", "be", "bes", "bot", 
-	"ces", "ch", "chre", "cial", "cian", "ck", "cker", "ction", 
-	"dom", "dome", "don", "drone", "dth", "dy", 
-	"fication", "form", "fresh", "ft", "fy", 
-	"ges", "gh", "ghst", "ght", "glo", "go", "gan", "gon", "gram", 
-	"ja", "jam", "jo", 
-	"k", "ker", 
-	"lay", "ld", "lk", "llator", "ller", "lo", "lobe", "log", "lope", "lour", "lst", "lter", 
-	"mage", "main", "maker", "man", "mat", "mator", "me", "mium", "mix", "mmer", "mst", "mum", 
+	"back", "bank", "be", "beha", "bes", "bone", "bot", 
+	"ces", "ch", "chon", "chre", "cial", "cian", "citor", "city", "ck", "cker", "ction", "ctor",
+	"de", "dom", "dome", "don", "drone", "dth", "dule", "dy", 
+	"faction", "ffer", "fication", "flix", "form", "fract", "ft", "fy", 
+	"ges", "gh", "ghst", "ght", "glo", "gner", "go", "gan", "gon", "gram", 
+	"hand", "hex", "hive", 
+	"ja", "jack", "jam", "jo", "ju",
+	"k", "ker", "ki", "klang", "knot", "ku", 
+	"lange", "lay", "ld", "le", "let", "lix", "lk", "llator", "ller", "lo", "load", "lobe", "lode", "log", "lope", "lour", "lst", "lter", 
+	"mage", "main", "maker", "man", "mancy", "mant", "mat", "mator", "me", 
+	"mic", "ming", "mion", "mium", "mix", "mmer", "mst", "mum", 
 	"n", "nal", "nance", "nation", "nc", "nch", "ncy", "nd", "ndom", "nds", "ne", "nes", "ness", "ng", "nge", "ngine", "ngs", "ngual", 
-	"ning", "nion", "nk", "nnch", "no", "node", "nos", "notion", 
-	"nt", "nter", "nto", 
-	"pass", "pe", "pth", "put", 
+	"nics", "ning", "nion", "nk", "nnch", 
+	"no", "node", "non", "norm", "nos", "notion", "nout", "nst", 
+	"nt", "nts", "nter", "nto", 
+	"pass", "pe", "phax", "phics", "pth", "put", 
 	"q",
-	"r", "rator", "re", "reo", "rich", "rim", "rity", "rl", "rle", "rn", "rm", "ron", "rse", "rt", "rte", "rus", "ry",
-	"sc", "se", "sh", "sion", "sium", "sm", "smo", "sn", "son", "ssion", "st", "stab", "stal", 
-	"t", "tache", "tal", "tch", "te", "ter", "th", "tier", "tion", "tior", "tode", "ton", "tone", "tor", 
+	"r", "rach", "rasm", "rath", "rator", "rce", "re", "rella", "rence", "reo", "rial", "rich", "rim", "rish", "rity", "rix", "rl", "rle", "rn", "rm", "ron", "rse", "rt", "rte", "rus", "ry",
+	"sc", "scape", "se", "sh", "shub", "sion", "sium", "sm", "smo", "sn", "son", "ssion", "st", "stack", "stal", "stan", "state", "step", "stor", 
+	"t", "tache", "tal", "tch", "te", "ter", "th", "tier", "tion", "tior", "tler", "tode", "tome", "ton", "tone", "tor", 
 	"tre", "tron", "tten", "tude", "ty", "type", "tzsche", 
-	"v", "ve", "vel", "ver", "vert", 
+	"v", "vat", "vator", "ve", "vel", "ver", "vert", "viour",
 	"x", "xion", "xer", 
 	"y", 
-	"z",
+	"z", "zex", "zone", 
 ];
 
 function pick(arr) {
@@ -62,7 +69,11 @@ function pick(arr) {
 
 var paramname1 = function(parts) {
 	var name = "";
-	name = name + pick(startings); 	
+	if (random() < 0.5) {
+		name = name + pick(startings); 	
+	} else {
+		name = name + pick(vowels);
+	}
 	for (var i=1; i<parts; i++) {	
 		name = name + pick(consonants); 	
 		name = name + pick(vowels); 
--- synth.js
+++ synth.js
@@ -2,119 +2,6 @@ Gibberish.init();
 Gibberish.Time.export();
 Gibberish.Binops.export();
 
-var apply_lfo = function(synth, ugen) {
-	if (synth.inputs.length > 0) {
-	
-		// get an existing input:
-		var index = Math.floor(random() * synth.inputs.length);
-		var input = synth.inputs[index];
-	
-		// apply to this input:
-		input.replace(ugen);
-	
-		// now remove it:
-		synth.inputs.splice(index, 1);
-		
-	} else {
-	
-		// use it as output:
-		synth.outputs.push(ugen);
-	}
-	
-	return ugen;
-}
-
-var standard_osc = function(synth, ugen) {
-	// must add ugen before adding inputs
-	var ugen = apply_lfo(synth, ugen);
-	
-	synth.inputs.push({ 
-		type: "control", 
-		min: 0, max: 3200,
-		replace: function(o) { ugen.frequency = Mul(o, 3200); },
-		ui: new Slider(function(v) { ugen.frequency = v; }),
-	});
-	
-	synth.inputs.push({ 
-		type: "audible", 
-		min: -1, max: 1, 
-		default: 1,
-		replace: function(o) { ugen.amp = Add(-1, Mul(o, 2)); },
-		ui: new Slider(function(v) { ugen.amp = v; }),
-	});
-	
-	return ugen;
-}
-
-// modulators (oscillators):
-
-var sine = function(synth) {	
-	var ugen = new Gibberish.Sine();
-	return standard_osc(synth, ugen);
-}
-
-var saw = function(synth) {
-	var ugen = new Gibberish.Saw();
-	return standard_osc(synth, ugen);
-}
-
-
-var saw3 = function(synth) {
-	var ugen = new Gibberish.Saw3();
-	return standard_osc(synth, ugen);
-}
-
-var tri = function(synth) {
-	var ugen = new Gibberish.Triangle();
-	return standard_osc(synth, ugen);
-}
-
-// seems really noisy
-var pwm = function(synth) {
-	var ugen = new Gibberish.PWM();
-	standard_osc(synth, ugen);
-	synth.inputs.push({ 
-		type: "audible", 
-		min: 0, max: 1, 
-		default: 1,
-		replace: function(o) { ugen.pulsewidth = o; },
-		ui: new Slider(function(v) { ugen.pulsewidth = v; }),
-	});
-	return ugen;
-}
-
-var noise = function(synth) {
-	var ugen = new Gibberish.Noise();
-	apply_lfo(synth, ugen);
-	synth.inputs.push({ 
-		type: "audible", 
-		min: -1, max: 1, 
-		default: 1,
-		replace: function(o) { ugen.amp = Add(-1, Mul(o, 2)); },
-		ui: new Slider(function(v) { ugen.amp = v; }),
-	});
-	return ugen;
-}
-
-var modulators = [
-	sine, 
-	sine, saw, saw3, tri, 
-	sine, saw, saw3, tri, 
-	pwm, 
-	//noise,
-];
-
-var modulate = function(synth) {
-	return pick(modulators)(synth);
-}
-
-// Add, Mul, Sub, Div, Mod, Abs, Sqrt, Pow
-// check issues of div/mod 0)
-// check issues of sqrt(-ve) -> sqrt(abs(x))
-// also non-integer power issues of negatives. pow(abs(x), e)
-
-// Synth, PolySynth, Synth2, PolySynth2, FMSynth, PolyFM, MonoSynth, KarplusStrong: all triggered by .note() events.
-
 ////////////////////////////////////////////////////////////////////////////////
 
 var maxdelay = 44100 / 2;
@@ -354,6 +241,127 @@ var fxs = [
 var fx = function(synth, ugen) {
 	return pick(fxs)(synth, ugen);
 }
+
+////////////////////////////////////////////////////////////////////////////////
+
+var apply_lfo = function(synth, ugen) {
+	if (synth.inputs.length > 0) {
+	
+		// get an existing input:
+		var index = Math.floor(random() * synth.inputs.length);
+		var input = synth.inputs[index];
+	
+		// apply to this input:
+		input.replace(ugen);
+	
+		// now remove it:
+		synth.inputs.splice(index, 1);
+		
+	} else {
+	
+		// use it as output:
+		synth.outputs.push(ugen);
+	}
+	
+	return ugen;
+}
+
+var standard_osc = function(synth, ugen) {
+	// must add ugen before adding inputs
+	var ugen = apply_lfo(synth, ugen);
+	
+	synth.inputs.push({ 
+		type: "control", 
+		min: 0, max: 3200,
+		replace: function(o) { ugen.frequency = Mul(o, 3200); },
+		ui: new Slider(function(v) { ugen.frequency = v; }),
+	});
+	
+	synth.inputs.push({ 
+		type: "audible", 
+		min: -1, max: 1, 
+		default: 1,
+		replace: function(o) { ugen.amp = Add(-1, Mul(o, 2)); },
+		ui: new Slider(function(v) { ugen.amp = v; }),
+	});
+	
+	return ugen;
+}
+
+// modulators (oscillators):
+
+var sine = function(synth) {	
+	var ugen = new Gibberish.Sine();
+	return standard_osc(synth, ugen);
+}
+
+var saw = function(synth) {
+	var ugen = new Gibberish.Saw();
+	return standard_osc(synth, ugen);
+}
+
+
+var saw3 = function(synth) {
+	var ugen = new Gibberish.Saw3();
+	return standard_osc(synth, ugen);
+}
+
+var tri = function(synth) {
+	var ugen = new Gibberish.Triangle();
+	return standard_osc(synth, ugen);
+}
+
+// seems really noisy
+var pwm = function(synth) {
+	var ugen = new Gibberish.PWM();
+	standard_osc(synth, ugen);
+	synth.inputs.push({ 
+		type: "audible", 
+		min: 0, max: 1, 
+		default: 1,
+		replace: function(o) { ugen.pulsewidth = o; },
+		ui: new Slider(function(v) { ugen.pulsewidth = v; }),
+	});
+	return ugen;
+}
+
+var noise = function(synth) {
+	var ugen = new Gibberish.Noise();
+	apply_lfo(synth, ugen);
+	synth.inputs.push({ 
+		type: "audible", 
+		min: -1, max: 1, 
+		default: 1,
+		replace: function(o) { ugen.amp = Add(-1, Mul(o, 2)); },
+		ui: new Slider(function(v) { ugen.amp = v; }),
+	});
+	return ugen;
+}
+
+var modulators = [
+	sine, 
+	sine, saw, saw3, tri, 
+	sine, saw, saw3, tri, 
+	pwm, 
+	//noise,
+];
+
+var modulate = function(synth) {
+	var ugen = pick(modulators)(synth);
+	if (random() < 0.1) {
+		ugen = fx(synth, ugen);
+	}
+	return ugen;
+}
+
+// Add, Mul, Sub, Div, Mod, Abs, Sqrt, Pow
+// check issues of div/mod 0)
+// check issues of sqrt(-ve) -> sqrt(abs(x))
+// also non-integer power issues of negatives. pow(abs(x), e)
+
+// Synth, PolySynth, Synth2, PolySynth2, FMSynth, PolyFM, MonoSynth, KarplusStrong: all triggered by .note() events.
+
+
 
 var root = function(synth) {
 	var ugen = modulate(synth);
